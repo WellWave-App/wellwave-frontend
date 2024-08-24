6504c1b2cf619b5d5fa858a0027dfa2142c26806
Pimchanok Ritchuay <pimchanok.ritchuay@gmail.com>
2024-08-24 15:07:36 +0700

    add button for health connect

--- lib/features/home/presentation/screen/splash_screen.dart
+++ lib/features/home/presentation/screen/splash_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:wellwave_frontend/config/constants/app_pages.dart';
 import 'package:wellwave_frontend/features/home/presentation/screen/home_screen.dart';
+import 'package:wellwave_frontend/features/home/presentation/screen/health_connect.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -10,12 +11,27 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            context.goNamed(AppPages.homeName);
-
-          },
-          child: Text('Go to Home'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                context.goNamed(AppPages.homeName);
+              },
+              child: const Text('Go to Home'),
+            ),
+            const SizedBox(height: 20), // Add some spacing between buttons
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => HealthConnect()),
+                );
+              },
+              child: const Text('Go to Health Connect'),
+            ),
+          ],
         ),
       ),
     );
