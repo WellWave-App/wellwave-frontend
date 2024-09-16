87c6d6697a3e8fcb458e2ae3bfc969bb82a0da86
Aratcha Pinprayoon <aratcha.mm@gmail.com>
2024-09-17 02:37:32 +0700

    fix: edit fontsize

--- lib/common/widget/app_bar.dart
+++ lib/common/widget/app_bar.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:step_progress_indicator/step_progress_indicator.dart';
 import '../../config/constants/app_colors.dart';
 
 class CustomAppBar extends AppBar {
@@ -16,7 +16,7 @@ class CustomAppBar extends AppBar {
   }) : super(
           title: Text(
             title ?? '',
-            style: Theme.of(context).textTheme.titleMedium?.copyWith(
+            style: Theme.of(context).textTheme.headlineMedium?.copyWith(
                   color: AppColors.blackColor,
                 ),
           ),
@@ -49,5 +49,65 @@ class CustomAppBar extends AppBar {
                 color: textColor ?? Colors.black,
               ),
           ],
+        );
+}
+
+class CustomAppBarWithStep extends AppBar {
+  CustomAppBarWithStep({
+    super.key,
+    required BuildContext context,
+    required bool onLeading,
+    required int totalSteps,
+    required int currentStep,
+    Color? textColor,
+    Color? bgColor,
+    Function? onBackPressed,
+    Function? action,
+    IconData? actionIcon,
+  }) : super(
+          backgroundColor: Colors.transparent,
+          automaticallyImplyLeading: false,
+          leading: onLeading
+              ? GestureDetector(
+                  onTap: () {
+                    if (onBackPressed != null) {
+                      onBackPressed();
+                    } else {
+                      Navigator.of(context).pop();
+                    }
+                  },
+                  child: Icon(
+                    Icons.arrow_back_ios_new_sharp,
+                    size: 16,
+                    color: textColor ?? Colors.black,
+                  ),
+                )
+              : const SizedBox(width: 48),
+          actions: [
+            if (action != null)
+              IconButton(
+                onPressed: () => action(),
+                icon: Icon(actionIcon ?? Icons.circle_outlined),
+                color: textColor ?? Colors.black,
+              ),
+            const SizedBox(width: 48),
+          ],
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 200,
+                height: 20,
+                child: StepProgressIndicator(
+                  totalSteps: totalSteps,
+                  currentStep: currentStep + 1,
+                  size: 4,
+                  padding: 0,
+                  selectedColor: AppColors.primaryColor,
+                  unselectedColor: AppColors.lightgrayColor,
+                ),
+              ),
+            ],
+          ),
         );
 }
