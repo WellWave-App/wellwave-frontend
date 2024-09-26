64f0454466c73958775a51ac3088008922a4fc81
Pimchanok Ritchuay <pimchanok.ritchuay@gmail.com>
2024-09-26 19:32:29 +0700

    fix : make weekly button can click once

--- lib/features/logs/presentation/widget/weekly_logs.dart
+++ lib/features/logs/presentation/widget/weekly_logs.dart
@@ -21,26 +21,47 @@ class _WeeklyLogsState extends State<WeeklyLogs> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
+          BlocBuilder<LogsBloc, LogsState>(
+            builder: (context, state) {
+              bool logsRecorded = false;
+              if (state is LogsLoadSuccess) {
+                logsRecorded = state.logslist.isNotEmpty;
+              }
+
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(AppStrings.weeklyLogsText,
-                      style: Theme.of(context)
-                          .textTheme
-                          .headlineMedium
-                          ?.copyWith(fontWeight: FontWeight.bold)),
-                ],
-              ),
-              const Column(
-                children: [
-                  InputButton(
-                    buttonText: AppStrings.dataRecordingText,
+                  Column(
+                    children: [
+                      Text(AppStrings.weeklyLogsText,
+                          style: Theme.of(context)
+                              .textTheme
+                              .headlineMedium
+                              ?.copyWith(fontWeight: FontWeight.bold)),
+                    ],
                   ),
+                  logsRecorded
+                      ? Container(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 8.0, horizontal: 16.0),
+                          decoration: BoxDecoration(
+                            color: Colors.grey[400], // Gray background
+                            borderRadius: BorderRadius.circular(16.0),
+                          ),
+                          child: Text(
+                            AppStrings.dataRecordingCompletedText, // Already recorded text
+                            style: Theme.of(context)
+                              .textTheme
+                              .titleSmall
+                              ?.copyWith(color: Colors.white)),
+                          
+                        )
+                      : const InputButton(
+                          buttonText: AppStrings.dataRecordingText,
+                        ),
                 ],
-              )
-            ],
+              );
+            },
           ),
           const SizedBox(height: 16),
           BlocBuilder<LogsBloc, LogsState>(
@@ -49,17 +70,22 @@ class _WeeklyLogsState extends State<WeeklyLogs> {
                 return const Center(child: CircularProgressIndicator());
               } else if (state is LogsLoadSuccess) {
                 double weight = 0.0;
-                double hdl = 0.0;
-                double ldl = 0.0;
+                double waistLine = 0.0;
+                // double hdl = 0.0;
+                // double ldl = 0.0;
 
                 for (var log in state.logslist) {
                   if (log?.logName == 'WEIGHT_LOG') {
                     weight = log?.value ?? 0.0;
-                  } else if (log?.logName == 'HDL_LOG') {
-                    hdl = log?.value ?? 0.0;
-                  } else if (log?.logName == 'LDL_LOG') {
-                    ldl = log?.value ?? 0.0;
-                  }
+                  } else if (log?.logName == 'WAIST_LINE_LOG') {
+                    waistLine = log?.value ?? 0.0;
+                  } 
+                  
+                  // else if (log?.logName == 'HDL_LOG') {
+                  //   hdl = log?.value ?? 0.0;
+                  // } else if (log?.logName == 'LDL_LOG') {
+                  //   ldl = log?.value ?? 0.0;
+                  // }
                 }
 
                 return Column(
@@ -77,23 +103,33 @@ class _WeeklyLogsState extends State<WeeklyLogs> {
                     ),
                     const SizedBox(height: 16),
                     Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         WeeklyLogsCard(
-                          title: AppStrings.hdlText,
-                          value: hdl,
-                          unit: AppStrings.mgPerDlText,
-                          chart: const LineChartSample2(),
-                        ),
-                        const SizedBox(width: 16),
-                        WeeklyLogsCard(
-                          title: AppStrings.ldlText,
-                          value: ldl,
-                          unit: AppStrings.mgPerDlText,
+                          title: AppStrings.waistLineText,
+                          value: waistLine,
+                          unit: AppStrings.cmText,
                           chart: const LineChartSample2(),
                         ),
                       ],
                     ),
+                    // Row(
+                    //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    //   children: [
+                    //     WeeklyLogsCard(
+                    //       title: AppStrings.hdlText,
+                    //       value: hdl,
+                    //       unit: AppStrings.mgPerDlText,
+                    //       chart: const LineChartSample2(),
+                    //     ),
+                    //     const SizedBox(width: 16),
+                    //     WeeklyLogsCard(
+                    //       title: AppStrings.ldlText,
+                    //       value: ldl,
+                    //       unit: AppStrings.mgPerDlText,
+                    //       chart: const LineChartSample2(),
+                    //     ),
+                    //   ],
+                    // ),
                   ],
                 );
               } else if (state is LogsError) {
