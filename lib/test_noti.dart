b1c6799b2f9f096cea66a4e977a52439bdf189a0
Pimchanok Ritchuay <pimchanok.ritchuay@gmail.com>
2025-01-22 04:07:28 +0700

    add : add firebase but have gradle error

--- lib/test_noti.dart
+++ lib/test_noti.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 
 import 'common/widget/app_bar.dart';
 
-class TestNotiScreen extends StatelessWidget {
+class TestNotiScreen extends StatefulWidget {
   const TestNotiScreen({super.key});
 
+  @override
+  State<TestNotiScreen> createState() => _TestNotiScreenState();
+}
+
+class _TestNotiScreenState extends State<TestNotiScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
