68537ca2b546f581471886285980185ba1ac7251
aratchamm <aratchamm@gmail.com>
2025-02-11 15:52:14 +0700

    fix: change location folder img

--- lib/config/constants/app_images.dart
+++ lib/config/constants/app_images.dart
@@ -5,43 +5,70 @@ class AppImages {
   static const friendIcon = 'assets/icons/friend_icon.svg';
   static const articleIcon = 'assets/icons/article_icon.svg';
 
-  static const homeIconActive = 'assets/icons/home_active_icon.svg';
-  static const logIconActive = 'assets/icons/log_active_icon.svg';
-  static const friendIconActive = 'assets/icons/friend_active_icon.svg';
-  static const articleIconActive = 'assets/icons/article_active_icon.svg';
+  static const homeIconActive =
+      'assets/icons/health_assessment/home_active_icon.svg';
+  static const logIconActive =
+      'assets/icons/health_assessment/log_active_icon.svg';
+  static const friendIconActive =
+      'assets/icons/health_assessment/friend_active_icon.svg';
+  static const articleIconActive =
+      'assets/icons/health_assessment/article_active_icon.svg';
 
-  static const cameraIcon = 'assets/icons/camera.svg';
-  static const avatarDefaultIcon = 'assets/icons/avatar_default.svg';
-  static const checkmarkIcon = 'assets/icons/checkmark.svg';
+  static const cameraIcon = 'assets/icons/health_assessment/camera.svg';
+  static const avatarDefaultIcon =
+      'assets/icons/health_assessment/avatar_default.svg';
+  static const checkmarkIcon = 'assets/icons/health_assessment/checkmark.svg';
 
-  static const femaleImage = 'assets/icons/female_image.png';
-  static const maleImage = 'assets/icons/male_image.png';
-  static const femaleImageUnselected = 'assets/icons/female_gray_image.png';
-  static const maleImageUnselected = 'assets/icons/male_gray_image.png';
+  static const femaleImage = 'assets/icons/health_assessment/female_image.png';
+  static const maleImage = 'assets/icons/health_assessment/male_image.png';
+  static const femaleImageUnselected =
+      'assets/icons/health_assessment/female_gray_image.png';
+  static const maleImageUnselected =
+      'assets/icons/health_assessment/male_gray_image.png';
 
-  static const alcoholSometimesIcon = 'assets/icons/alcohol_sometimes.svg';
-  static const alcoholNeverIcon = 'assets/icons/alcohol_usedto.svg';
-  static const alcoholUsedtoIcon = 'assets/icons/alcohol_never.svg';
-  static const alcoholUsuallyIcon = 'assets/icons/alcohol_usually.svg';
-  static const famhistoryArteryIcon = 'assets/icons/famhistory_artery.svg';
-  static const famhistoryConfusedIcon = 'assets/icons/famhistory_confused.svg';
-  static const famhistoryDiabetesIcon = 'assets/icons/famhistory_diabetes.svg';
-  static const famhistoryHyperIcon = 'assets/icons/famhistory_hyper.svg';
+  static const alcoholSometimesIcon =
+      'assets/icons/health_assessment/alcohol_sometimes.svg';
+  static const alcoholNeverIcon =
+      'assets/icons/health_assessment/alcohol_usedto.svg';
+  static const alcoholUsedtoIcon =
+      'assets/icons/health_assessment/alcohol_never.svg';
+  static const alcoholUsuallyIcon =
+      'assets/icons/health_assessment/alcohol_usually.svg';
+  static const famhistoryArteryIcon =
+      'assets/icons/health_assessment/famhistory_artery.svg';
+  static const famhistoryConfusedIcon =
+      'assets/icons/health_assessment/famhistory_confused.svg';
+  static const famhistoryDiabetesIcon =
+      'assets/icons/health_assessment/famhistory_diabetes.svg';
+  static const famhistoryHyperIcon =
+      'assets/icons/health_assessment/famhistory_hyper.svg';
   static const famhistoryParalysisIcon =
-      'assets/icons/famhistory_paralysis.svg';
-  static const goalHealthyIcon = 'assets/icons/goal_healthy.svg';
-  static const goalLoseweightIcon = 'assets/icons/goal_loseweight.svg';
-  static const goalMuscleIcon = 'assets/icons/goal_muscle.svg';
+      'assets/icons/health_assessment/famhistory_paralysis.svg';
+  static const goalHealthyIcon =
+      'assets/icons/health_assessment/goal_healthy.svg';
+  static const goalLoseweightIcon =
+      'assets/icons/health_assessment/goal_loseweight.svg';
+  static const goalMuscleIcon =
+      'assets/icons/health_assessment/goal_muscle.svg';
 
-  static const famhistoryVascularIcon = 'assets/icons/famhistory_vascular.svg';
-  static const smokeUedtoIcon = 'assets/icons/smoke_usedto.svg';
-  static const smokeNeverIcon = 'assets/icons/smoke_never.svg';
-  static const smokeUsuallyIcon = 'assets/icons/smoke_usually.svg';
-  static const healthassessmentIMG = 'assets/icons/health_assessment_step1.svg';
-  static const healthassessmentBG = 'assets/images/health_assessment_bg.png';
-  static const recommendIcon = 'assets/icons/recommend.svg';
-  static const recommendPlanIcon = 'assets/icons/recommend_plan.svg';
-  static const healthconnectIcon = 'assets/icons/health_connect.svg';
+  static const famhistoryVascularIcon =
+      'assets/icons/health_assessment/famhistory_vascular.svg';
+  static const smokeUedtoIcon =
+      'assets/icons/health_assessment/smoke_usedto.svg';
+  static const smokeNeverIcon =
+      'assets/icons/health_assessment/smoke_never.svg';
+  static const smokeUsuallyIcon =
+      'assets/icons/health_assessment/smoke_usually.svg';
+  static const healthassessmentIMG =
+      'assets/icons/health_assessment/health_assessment_step1.svg';
 
-  static const healthassessmentGoalBG = 'assets/images/goal_bg.png';
+  static const recommendIcon = 'assets/icons/health_assessment/recommend.svg';
+  static const recommendPlanIcon =
+      'assets/icons/health_assessment/recommend_plan.svg';
+  static const healthconnectIcon =
+      'assets/icons/health_assessment/health_connect.svg';
+  static const healthassessmentBG =
+      'assets/icons/health_assessment/bg/health_assessment_bg.png';
+  static const healthassessmentGoalBG =
+      'assets/icons/health_assessment/bg/goal_bg.png';
 }
